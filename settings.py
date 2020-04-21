90794152ba3d5050b413bc144f26d3a661920673
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 13:14:09 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,10 +2,17 @@ class Settings(object):
     """Настройки игры."""
 
     def __init__(self):
+        # Настройки экрана
         self.fullscreen = True
-
         self.screen_width = 1200
         self.screen_height = 800
         self.background_color = (230, 230, 230)
 
+        # Настройки корабля
         self.ship_speed = 1.5
+
+        # Настройки снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
