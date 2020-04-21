644a228e7e8689fb57ead0b561366f818e83b82d
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 11:38:59 +0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings(object):
+    """Настройки игры."""
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.background_color = (230, 230, 230)
+
