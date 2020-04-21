025c66302496fa6a85eb9f07fb32a97ed7d07275
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 15:24:57 +0500

    Update alien.py

--- alien.py
+++ alien.py
@@ -6,6 +6,7 @@ class Alien(pygame.sprite.Sprite):
     def __init__(self, game):
         super().__init__()
         self.screen = game.screen
+        self.settings = game.settings
 
         # Загрузка изображения пришельца и назначение атрибута rect.
         self.image = pygame.image.load('images/alien.bmp')
@@ -17,3 +18,14 @@ class Alien(pygame.sprite.Sprite):
 
         # Сохранение точной горизонтальной позиции пришельца.
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Перемещает пришельца вправо."""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self) -> bool:
+        """Возвращает True, если пришелец находится у края экрана."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
