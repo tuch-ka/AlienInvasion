b299c992cb43b0a6122c203f72552b939ad726b3
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 13:53:04 +0500

    Create bullet.py

---
+++ bullet.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+class Bullet(pygame.sprite.Sprite):
+    """Класс для управления снарядами, выпущенными кораблем."""
+    def __init__(self, game):
+        super().__init__()
+        self.screen = game.screen
+        self.settings = game.settings
+        self.color = self.settings.bullet_color
+
+        # Создание снаряда
+        self.rect = pygame.Rect(
+            *game.ship.rect.midtop,
+            self.settings.bullet_width,
+            self.settings.bullet_height
+        )
+
+        # Хранение позиции снаряда в вещественном формате
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Перемещает снаряд вверх по экрану."""
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """отображение снаряда на экране."""
+        pygame.draw.rect(
+            self.screen,
+            self.color,
+            self.rect
+        )
