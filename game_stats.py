9c78daaa3f801b38f784e05b9d74dfa9c03e61c8
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 15:45:48 +0500

    Create game_stats.py

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+class GameStats(object):
+    """Отслеживание статистики для игры Alien Invasion."""
+    def __init__(self, game):
+        self.settings = game.settings
+        self.reset_stats()
+        self.game_active = True
+
+    def reset_stats(self):
+        """Инициализирует статистику, изменяющуюся в ходе игры."""
+        self.ships_left = self.settings.ship_limit
