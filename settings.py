8e49800998b6dc4877814dbc2c478f7c73bc01b6
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 13:10:11 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ class Settings(object):
     """Настройки игры."""
 
     def __init__(self):
+        self.fullscreen = True
+
         self.screen_width = 1200
         self.screen_height = 800
         self.background_color = (230, 230, 230)
