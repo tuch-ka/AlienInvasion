7c13335285658c5d283000380045dd94b88b68ab
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 16:16:52 +0500

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,44 @@
+import pygame
+
+
+class PlayButton(object):
+    """Кнопка начала игры"""
+
+    def __init__(self, game, message):
+        self.screen = game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Назначение размеров и свойств кнопок.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Построение объекта rect кнопки и выравнивание по центру экрана.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # Сообщение кнопки создается только один раз.
+        self._prep_msg(message)
+
+    def _prep_msg(self, message):
+        """Преобразует msg в прямоугольник и выравнивает текст по центру."""
+        self.msg_image = self.font.render(
+            message,
+            True,
+            self.text_color,
+            self.button_color,
+        )
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        """"Отображение пустой кнопки и вывод сообщения."""
+        self.screen.fill(
+            self.button_color,
+            self.rect,
+        )
+        self.screen.blit(
+            self.msg_image,
+            self.msg_image_rect,
+        )
