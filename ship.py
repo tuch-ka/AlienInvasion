a19bf552a475ca36f09d90587e347747743fd49f
Andrew Zakharov <57887043+tuch-ka@users.noreply.github.com>
2020-04-21 12:01:57 +0500

    Create ship.py

---
+++ ship.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class Ship(object):
+    """Класс управления кораблём."""
+    def __init__(self, game):
+        self.screen = game.screen
+        self.screen_rect = game.screen.get_rect()
+
+        self.image = pygame.image.load('images/ship.jpg')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
